a51f5d2750eb2855a949282669cde28089ce2c25
KOU050223 <kouhei20050223@icloud.com>
2024-10-17 21:11:50 +0900

    add ルーター

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ProductForm from './components/ProductForm';
 import SalesForm from './components/SalesForm';
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>文化祭 屋台販売会計アプリ</h1>
-      <h2>商品登録</h2>
-      <ProductForm />
-      <h2>売上伝票</h2>
-      <SalesForm />
-    </div>
-  )
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/products">商品登録</Link></li>
+          <li><Link to="/sales">売上伝票作成</Link></li>
+        </ul>
+      </nav>
+      
+      <Routes>
+        <Route path="/products" element={<ProductForm />} />
+        <Route path="/sales" element={<SalesForm />} />
+      </Routes>
+    </Router>
+    )
 }
 
 export default App
