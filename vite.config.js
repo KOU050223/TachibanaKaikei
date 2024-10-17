149098c9128fc6fba9623e315a84052f382918b1
KOU050223 <kouhei20050223@icloud.com>
2024-10-17 21:00:02 +0900

    add build_setting build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {            
+    'process.env': {}  
+  },  
+  base: process.env.GITHUB_PAGES
+        ? "tachibana-kaikei"
+        : "./",
   plugins: [react()],
 })
